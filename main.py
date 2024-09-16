d725cf68eab38bf1c06e0a031d8f05d43da2eb65
rhit-bryantlj <ljbryant8888@gmail.com>
2024-09-16 12:14:15 -0400

    added player object

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import pygame
 from constants import *
+from player import *
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    timer = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
 
     while True:
         for event in pygame.event.get():
@@ -12,7 +17,11 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        #limit to 60 fps
+        dt = timer.tick(60) / 1000
 
 
 if __name__ == "__main__":
